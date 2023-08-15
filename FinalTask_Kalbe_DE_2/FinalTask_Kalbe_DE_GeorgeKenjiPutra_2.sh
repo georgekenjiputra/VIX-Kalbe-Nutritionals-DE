f09f868b22a021ff06d66d0d1a45399104b9fb3e
georgekenjiputra <128347758+georgekenjiputra@users.noreply.github.com>
2023-08-15 14:23:00 +0700

    Add files via upload

---
+++ FinalTask_Kalbe_DE_2/FinalTask_Kalbe_DE_GeorgeKenjiPutra_2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Variables
+path="/hdfs/data/data1"
+name_of_directory="data1"
+filename_excel="daily_market_price.xlsx"
+source_dir="/local/data/market"
+target_dir="$path/$name_of_directory"
+log_file="$target_dir/move_log.txt"
+
+# Check if directory exists
+if [ -d "$target_dir" ]; then
+    echo "There is $name_of_directory Directory Exists!"
+
+    # Copy file from source to target directory
+    cp "$source_dir/$filename_excel" "$target_dir/"
+
+    if [ $? -eq 0 ]; then
+        echo "File Moved Successfully" > "$log_file"
+    else
+        echo "Failed to move the file" > "$log_file"
+    fi
+else
+    echo "$name_of_directory Directory Not Exists!"
+
+    # Create the directory
+    mkdir -p "$target_dir"
+
+    if [ $? -eq 0 ]; then
+        echo "Directory created successfully" > "$log_file"
+    else
+        echo "Failed to create the directory" > "$log_file"
+    fi
+fi
+
+# Variables
+# path="C:/Users/georg/OneDrive/Documents/Virtual Internship/Kalbe Nutritionals/Data Engineering"
+# name_of_directory="Week 04"
+# filename_excel="FinalTask_Kalbe_DE_GeorgeKenjiPutra_2.xlsx"
+# source_dir="C:/Users/georg/OneDrive/Documents/Virtual Internship/Kalbe Nutritionals/Data Engineering/Source"
+# target_dir="$path/$name_of_directory"
+# log_file="$source_dir/move_log.txt"
